cc061212f4dcbc9e93f4ac25087961853446329c
AllanOmondi <allanoduor38@gmail.com>
2025-10-23 21:04:04 +0300

    created main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello world'
+
+#products route
+@app.route('/products')
+def products():
+    return 'My products'
+
+# sales route
+@app.route('/sales')
+def sales():
+    return 'total sales'
+
+#stock route
+@app.route('/stock')
+def stock():
+    return 'total stock'
+
+
+
+
+
+
+
+
+
+
+
+app.run()
